5a70fabb8c20b06a90e82fa440f5b37712986be6
Junbum Kim <77708129+johnny9696@users.noreply.github.com>
2023-04-14 22:37:09 +0900

    Update 8989.py

--- 1~9999/8989.py
+++ 1~9999/8989.py
@@ -1,22 +1,27 @@
 """
 시간의 위치는 12로 나눈 나머지
+1시간에 움직이는 각도 30
+1분에 0.5도
 분침의 위치는 5로 나눈 몫
+분당 분침은 6도
 두수의 차이의 절대값 순으로 정렬
-
 """
-
 import sys
 input = sys.stdin.readline
 T= int(input())
 for _ in range(T):
-    time_list = list(map(str,input().split(' ')))
+    time_list = input()
+    time_list = list(time_list[:-1].split(' '))
     r_list =[]
     for i in time_list:
-        hh = float(int(i[:2])%12)
-        mm = int(i[3:])/5
-        angle = abs(hh-mm)
-        if angle>6:
-            angle = 12.0 - angle
-        r_list.append((angle, i))
+
+        hh = i[:2]
+        mm = i[3:]
+        h_angle = float((int(hh)%12)*30) + 0.5*float(mm)
+        m_angle = 6.0*float(mm)
+        angle = abs(h_angle - m_angle)
+        if angle >180.0:
+            angle = 360.0 - angle
+        r_list.append([angle, i])
     r_list.sort()
     print(r_list[2][1])
