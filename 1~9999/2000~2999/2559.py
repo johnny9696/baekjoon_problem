3ad8e80968edc68832a64a6347f45db6f7911805
Junbum Kim <77708129+johnny9696@users.noreply.github.com>
2023-04-29 22:26:25 +0900

    Update 2559.py

--- 1~9999/2000~2999/2559.py
+++ 1~9999/2000~2999/2559.py
@@ -2,8 +2,9 @@ import sys
 input = sys.stdin.readline
 N,K =map(int,input().split())
 temp_list = list(map(int,input().split()))
-max_temp = -sys.maxsize
-
+max_temp = sum(temp_list[:K])
+now = max_temp
 for i in range(K, N):
-    max_temp = max(max_temp, sum(temp_list[-K+i:i]))
+    now = now + temp_list[i] - temp_list[i-K]
+    max_temp = max(max_temp, now)
 print(max_temp)
