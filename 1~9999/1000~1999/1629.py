b3e8817079d22d164bff01158a8cca0719fd4822
Junbum Kim <77708129+johnny9696@users.noreply.github.com>
2023-05-01 22:38:14 +0900

    could has an error

--- 1~9999/1000~1999/1629.py
+++ 1~9999/1000~1999/1629.py
@@ -1,16 +1,9 @@
 import sys
 input = sys.stdin.readline
 a,b,c = map(int,input().split())
+remain_list = []
+re = a
+while re not in remain_list:
+    re = a%c
+    remain_list.append(re)
 
-def DAC(ta,tb):
-    if tb == 1:
-        return ta %c
-
-    temp  = DAC(ta,tb//2)
-
-    if tb%2 == 0:
-        return temp *temp %c
-    else:
-        return temp*temp*a%c
-
-print(DAC(a,b))
