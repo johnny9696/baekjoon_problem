bbf2d738b428111a98dce591145e31a70ebaf2cc
Junbum Kim <77708129+johnny9696@users.noreply.github.com>
2023-05-02 17:53:01 +0900

    Update 2776.py

--- 1~9999/2000~2999/2776.py
+++ 1~9999/2000~2999/2776.py
@@ -2,13 +2,14 @@ import sys
 input = sys.stdin.readline
 
 test_case = int(input())
+
 for _ in range(test_case):
     N = int(input())
-    n_list = list(map(int,input().split()))
+    n_list = set(map(int, input().split()))
     T = int(input())
     t_list = list(map(int,input().split()))
-    for data in t_list:
-        if data in n_list:
+    for i in t_list:
+        if i in n_list:
             print(1)
         else:
             print(0)
