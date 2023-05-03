0eab6edc503bfc1f746c8c291ea2cdb372e53e3d
Junbum Kim <77708129+johnny9696@users.noreply.github.com>
2023-05-03 21:04:11 +0900

    Update 1711.py

--- 1~9999/1000~1999/1711.py
+++ 1~9999/1000~1999/1711.py
@@ -7,11 +7,9 @@ result = 0
 for i in range(0,T-2):
     for j in range(i+1, T-1):
         for k in range(j+1,T):
-            triangle = [0,0,0]
-            triangle[0] = (p_list[i][0] - p_list[j][0]) ** 2 + (p_list[i][1] - p_list[j][1]) ** 2
-            triangle[1] = (p_list[i][0] - p_list[k][0]) ** 2 + (p_list[i][1] - p_list[k][1]) ** 2
-            triangle[2] = (p_list[k][0] - p_list[j][0]) ** 2 + (p_list[k][1] - p_list[j][1]) ** 2
-            triangle.sort()
-            if triangle[2] == triangle[0] + triangle[1]:
+            a = (p_list[i][0] - p_list[j][0]) ** 2 + (p_list[i][1] - p_list[j][1]) ** 2
+            b = (p_list[i][0] - p_list[k][0]) ** 2 + (p_list[i][1] - p_list[k][1]) ** 2
+            c = (p_list[k][0] - p_list[j][0]) ** 2 + (p_list[k][1] - p_list[j][1]) ** 2
+            if 2*max(a,b,c) == a+b+c:
                 result+=1
 print(result)
